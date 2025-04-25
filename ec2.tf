058afc89c2e126b7ed5741b664da8acd9accdf9e
Jakir Hossain <newjakir@gmail.com>
2025-04-26 00:17:14 +0600

    inital terraform setup

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-00695597b5e6018d3"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-e17dff87" ]
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
