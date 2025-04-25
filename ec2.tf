284b64b5ece3dba07c9405a0b6edf06becd65804
Jakir Hossain <newjakir@gmail.com>
2025-04-26 01:22:48 +0600

    ec2 instance config finilized

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-00695597b5e6018d3"
+  ami           = "ami-01938df366ac2d954"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ "sg-e17dff87" ]
+#  vpc_security_group_ids = [ "sg-e17dff87" ]
 
   tags = {
     Name = "ExampleAppServerInstance"
